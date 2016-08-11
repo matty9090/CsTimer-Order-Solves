77b41e6eed9ca15363876f61df18b0f3ad31fe22
matty9090 <mlowe.space@gmail.com>
2016-08-11 02:16:31 +0100

    Can select session

--- index.php
+++ index.php
@@ -3,29 +3,33 @@
 	
 	if(isset($_POST['solves'])) {
 		$s = json_decode($_POST['solves']);
-		$s = json_decode($s->session1);
-		$times = [];
 		
-		foreach($s as $v)
-			$times[] = $v[0][1] / 1000;
-		
-		asort($times);
-		
-		$cats = [];
-		
-		for($i = floor(min($times)); $i <= floor(max($times)); $i++) {
-			$t = [];
+		if($s) {
+			$n = 'session' . (int)$_POST['session'];
+			$s = json_decode($s->{$n});
+			$times = [];
 			
-			foreach($times as $time)
-				if(floor($time) == $i)
-					$t[] = number_format($time, 3);
+			foreach($s as $v)
+				$times[] = $v[0][1] / 1000;
 			
-			$cats[$i] = $t;
-		}
-		
-		foreach($cats as $k => $v) {
-			$solves .= '<h4>' . $k . ' seconds (' . count($v) . ')</h4>';
-			$solves .= implode(', ', $v) . '<br /><br />';
+			asort($times);
+			
+			$cats = [];
+			
+			for($i = floor(min($times)); $i <= floor(max($times)); $i++) {
+				$t = [];
+				
+				foreach($times as $time)
+					if(floor($time) == $i)
+						$t[] = number_format($time, 3);
+				
+				$cats[$i] = $t;
+			}
+			
+			foreach($cats as $k => $v) {
+				$solves .= '<h4>' . $k . ' seconds (' . count($v) . ')</h4>';
+				$solves .= implode(', ', $v) . '<br /><br />';
+			}
 		}
 	}
 ?>
@@ -56,6 +60,12 @@
 				background-color: #f1f1f1;
 			}
 			
+			input[type='number'] {
+				width: 40px;
+				font-size: 16px;
+				padding: 2px;
+			}
+			
 			textarea {
 				display: block;
 				width: 100%;
@@ -77,9 +87,9 @@
 		
 			<div class="box">
 				<form action="index.php" method="post">
+					Session: <input type="number" name="session" min="1" value="1" /><br /><br />
 					<textarea name="solves"><?php echo (isset($_POST['solves'])) ? $_POST['solves'] : ''; ?></textarea>
-					<br />
-					<input type="submit" name="parse" value="Go" />
+					<br /><input type="submit" name="parse" value="Go" />
 				</form>
 			</div>
 		</div>
